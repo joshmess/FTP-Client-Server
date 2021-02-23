e046f3c5ee355185013989f42795ca6f963ffe09
Alex Holmes <alexhholmes@protonmail.com>
2021-02-22 20:03:38 -0500

    Change connection listener to create connections for either normal/terminate ports

---
+++ ConnectionListener.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Listens for new connections on the normal or terminate connection port and creates a new
+ * thread for that FTP connection.
+ */
+class ConnectionListener implements Runnable {
+    public enum ListenerType {
+        NORMAL      ("NORMAL"),
+        TERMINATE   ("TERMINATE");
+
+        private final String name;
+
+        ListenerType(String name) {
+            this.name = name;
+        }
+    }
+
+    int port;
+    ListenerType type;
+
+    ConnectionListener(int nPort, ListenerType type) {
+        port = nPort;
+        this.type = type;
+        System.out.printf("Creating thread: %s Port Listener\n", type.name);
+    }
+
+    public void run() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.printf("Listening on %s port...\n", type.name);
+
+            while (true) {
+                try {
+                    Socket clientSocket = serverSocket.accept();
+                    if (type == ListenerType.NORMAL) {
+                        new NormalConnection(clientSocket);
+                    } else {
+                        new TerminateConnection(clientSocket);
+                    }
+                    System.out.printf("Connection established on %s port.\n", type.name);
+                } catch (IOException e) {
+                    System.err.printf("[ERROR] Unable to accept %s connection. Continuing to accept new connections.\n", type.name);
+                }
+            }
+        } catch (IOException e) {
+            System.err.printf("[ERROR] Unable to create %s port socket.\n", type.name);
+        } catch (IllegalArgumentException e) {
+            System.err.printf("[ERROR] Invalid server %s port number. Value must be 0 - 65535.\n", type.name);
+        }
+    }
+}
